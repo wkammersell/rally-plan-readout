2c7f5de7990518d9f89017dd9a5fb9387b11f1c6
William Kammersell <william.kammersell@ca.com>
2019-02-27 06:19:27 -0700

    Fetch SubObjectives with Features

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ Ext.define('CustomApp', {
 			xype: 'box',
 			border: 0
 		});
+		//TODO Fetch Portfolio Item Types
 		app.callParent( arguments );
     },
 	
@@ -19,13 +20,16 @@ Ext.define('CustomApp', {
 	onScopeChange: function( scope ) {
 		app.callParent( arguments );
 		// Show loading message
-		app._myMask = new Ext.LoadMask(Ext.getBody(), {msg:"Fetching Your Plan... Please wait."});
+		app._myMask = new Ext.LoadMask(Ext.getBody(), { msg: "Fetching Your Plan... Please wait." } );
 		app._myMask.show();
+		
 		var scope = app.getContext().getTimeboxScope().getRecord();
 		app.fetchStories( scope );
 	},
 	
+	//TODO: Get Defects too
 	fetchStories: function( scope ) {
+		console.log( 'Fetching Stories..' );
 		var filters = [];
 		var releaseFilter = Ext.create('Rally.data.wsapi.Filter', {
 			property : 'Release',
@@ -38,8 +42,9 @@ Ext.define('CustomApp', {
 			'Rally.data.wsapi.Store',
 			{
 				model: 'UserStory',
-				fetch: ['FormattedID','Name','Project','Release','Feature'],
+				fetch: ['FormattedID','Name','Project','Feature'],
 				context: app.getContext().getDataContext(),
+				//TODO: Do we need to load more than 2000 items?
 				pageSize: 2000,
 				limit: 2000
 			},
@@ -54,23 +59,88 @@ Ext.define('CustomApp', {
 			callback: function( records, operation ) {
 				if( operation.wasSuccessful() ) {
 					_.each( records, function( record ) {
-						var storyDetails = {};
-						storyDetails.name = record.data.Name;
-						storyDetails.formattedID = record.data.FormattedID;
 						
+						// TODO: Fetch Dependencies and Risks. See https://raw.githubusercontent.com/wkammersell/keep-or-sweep/master/App.js with Discussion loading for an example
+						
+						// Add story data to the lookup by feature
 						if( record.data.Feature ) {
-							if( storyToFeatureMap[ record.data.Feature._ref ] === undefined ) {
-								storyToFeatureMap[ record.data.Feature._ref ] = [];
+							if( storyToFeatureMap[ record.data.Feature.FormattedID ] === undefined ) {
+								storyToFeatureMap[ record.data.Feature.FormattedID ] = [];
 							}
-							storyToFeatureMap[ record.data.Feature._ref ].push( storyDetails );
+							storyToFeatureMap[ record.data.Feature.FormattedID ].push( record.data );
 						} else {
-							storyToFeatureMap.Unaligned.push( storyDetails );
+							storyToFeatureMap.Unaligned.push( record.data );
 						}
 						
 						console.log( storyToFeatureMap );
 					}, app );
+					app.fetchStoriedFeatures( scope, storyToFeatureMap );
 				}
 			}
 		});	
+	},
+	
+	fetchStoriedFeatures: function( scope, storyToFeatureMap ) {
+		console.log( 'Fetching Storied Features...' );
+		var featureToSubObjectiveMap = {};
+		featureToSubObjectiveMap.Unaligned = [];
+		featureToSubObjectiveMap.Unaligned.push( { 'storyToFeatureMap': storyToFeatureMap[ 'Unaligned' ] } );
+		
+		_.each( Object.keys( storyToFeatureMap ), function( featureKey ) {
+			console.log( 'Fetching ' + featureKey + ' ...' );
+			var filters = [];
+			var idFilter = Ext.create('Rally.data.wsapi.Filter', {
+				property : 'FormattedID',
+				operator: '=',
+				value: featureKey
+			});
+			filters.push( idFilter );
+			
+			var store = Ext.create(
+				'Rally.data.wsapi.Store',
+				{
+					model: 'PortfolioItem/Feature',
+					fetch: ['FormattedID','Name','Project','Release','Parent'],
+					context: app.getContext().getDataContext(),
+					pageSize: 2000,
+					limit: 2000
+				},
+				app
+			);
+			store.addFilter( filters, false );
+			console.log( filters );
+			
+			store.loadPage(1, {
+				scope: app,
+				callback: function( records, operation ) {
+					if( operation.wasSuccessful() && records.length > 0 ) {
+						var record = records[ 0 ];
+						console.log( record.data );
+						record.data.storyToFeatureMap = storyToFeatureMap[ record.data.FormattedID ];
+						if( record.data.Parent ) {
+							if( featureToSubObjectiveMap[ record.data.Parent.FormattedID ] === undefined ) {
+								featureToSubObjectiveMap[ record.data.Parent.FormattedID ] = [];
+							}
+							featureToSubObjectiveMap[ record.data.Parent.FormattedID ].push( record.data );
+						} else {
+							featureToSubObjectiveMap.Unaligned.push( record.data );
+						}
+						
+						// See if we're done and can move on to the next loading
+						if( featureToSubObjectiveMap.length == storyToFeatureMap.length ) {
+							console.log( featureToSubObjectiveMap );
+							console.log( 'NEXT!!!' )
+						}
+					}
+				}
+			});
+		}, app );
 	}
+	
+	// Load Features that have no Stories
+	// Load Sub-Objectives for the Features
+	// Load Sub-Objectives that have no Stories
+	// Load Objectives for the Sub-Objectives
+	// Load Objectives that have no Sub-Objectives
+	// Display the awesomeness
 });
